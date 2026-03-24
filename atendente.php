3f1e7ad79815f92925531029dd7fcbacb123262f
mitchelmicro-dotcom <emailmitchelmicro@gmail.com>
2026-03-24 15:35:55 -0300

    https://github.com/mitchelmicro-dotcom/projeto_painel_senhas.git

---
+++ atendente.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Painel Atendente - Sistema de Senhas</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Roboto', sans-serif;
+                background-color: #f8f9fa;
+            }
+        </style>
+    </head>
+
+    <body>
+        
+    </body>
+</html>
